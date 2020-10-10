b0d89492b6d22172afd273a9201ec2add658c117
Zhenhua yu <zyu0628@gmail.com>
2020-10-10 23:12:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,5 @@
 var playlist = { artistName :"songTitle"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({},playlist, {playlist :["artistName","songTitle"]})
-  updatePlaylist
-} 
+  platlist[artistName] = songTitle
+}
